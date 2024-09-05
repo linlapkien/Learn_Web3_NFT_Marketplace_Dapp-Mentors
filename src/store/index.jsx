e727ba0e693c29dfcf30d01dd35b1e3fed585deb
Lin Lập Kiến <linlapkien@gmail.com>
2024-09-05 10:55:27 +0700

    added Alert announce component

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -4,12 +4,27 @@ const { setGlobalState, useGlobalState, getGlobalState } = createGlobalState({
   modal: 'scale-0',
   showModal: 'scale-0',
   updateModal: 'scale-0',
-  loading: { show: true, msg: '' },
+  loading: { show: false, msg: '' },
+  alert: { show: true, msg: 'Minting failed', color: 'red' },
 });
+
+const setAlert = (msg, color = 'red') => {
+  setGlobalState('loading', { show: false, msg: '' });
+  setGlobalState('alert', { show: true, msg, color });
+  setTimeout(() => {
+    setGlobalState('alert', { show: false, msg, color });
+  }, 6000);
+};
 
 const setLoadingMsg = (msg) => {
   const loading = getGlobalState('loading');
   setGlobalState('loading', { ...loading, msg });
 };
 
-export { setGlobalState, useGlobalState, getGlobalState, setLoadingMsg };
+export {
+  setGlobalState,
+  useGlobalState,
+  getGlobalState,
+  setLoadingMsg,
+  setAlert,
+};
